9d0b8c518248c7dcc48a4b2133f3b4c490139639
Udit Sanghi <42933260+T1duS@users.noreply.github.com>
2018-12-13 20:30:13 +0530

    Added database for stripper

--- issue_maker.py
+++ issue_maker.py
@@ -5,6 +5,7 @@ import os
 from requests.auth import HTTPBasicAuth
 from flask import Flask, request
 from unidecode import unidecode
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 username = os.environ['USERNAME']
@@ -12,6 +13,30 @@ gh_token = 'GH_TOKEN'
 token = ''
 t_expiry = 0
 
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{username}.mysql.pythonanywhere-services.com/{username}${databasename}".format(
+    username="USERNAME",
+    password="PASSWORD",
+    databasename="DB_NAME",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Lyrics(db.Model):
+    __tablename__ = "strip"
+
+    id = db.Column(db.Integer, primary_key=True)
+    song = db.Column(db.String(4096))
+    artist = db.Column(db.String(4096))
+    stripper = db.Column(db.String(4096))
+
+    def __init__(self, song, artist, stripper):
+        self.song = song
+        self.artist = artist
+        self.stripper = stripper
+
+db.create_all()
 
 def update_token():
     global token, t_expiry
@@ -104,3 +129,24 @@ def update():
 
         return "fak u nibba that's a fishy request \nIf you feel there's an error, open a " \
                "ticket at https://github.com/aadibajpai/SwagLyrics-For-Spotify/issues"
+    
+@app.route("/stripper", methods=["GET", "POST"])
+def stripper():
+    if request.method == 'POST':
+        song = request.form['song']
+        artist = request.form['artist']
+        lyrics = Lyrics.query.filter(Lyrics.song==song).filter(Lyrics.artist==artist).first()
+        if lyrics:
+            return lyrics.stripper
+        else:
+            return "No Stripper Found"
+
+@app.route("/add_song", methods=["GET", "POST"])
+def add_song():
+    song = request.form['song']
+    artist = request.form['artist']
+    stripper = request.form['stripper']
+    lyrics = Lyrics(song=song, artist=artist, stripper=stripper)
+    db.session.add(lyrics)
+    db.session.commit()
+    return "Added"
